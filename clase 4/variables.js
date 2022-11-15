e3d6667c9a2370f15b121f12a11ff2a30e7e97b6
maritzaruano01 <maritza_dy@hotmail.com>
2022-11-15 14:24:50 -0500

    cambio snuevos

---
+++ clase 4/variables.js
@@ -0,0 +1,27 @@
+//Tipos de Varibles 
+// var i= 10;
+// const f = 10;
+// let g=2;
+// if(i==undefined){
+//     i=4;
+// }else{
+//     i=5;
+// }
+// console.log(i)
+// f++;
+
+
+
+
+
+//Estructuras de control
+var k = 8;
+
+
+if (k==6){
+    k++
+}else if(k==7){
+    alert('Hola')
+}else{
+    console.log('Morchi')
+}
