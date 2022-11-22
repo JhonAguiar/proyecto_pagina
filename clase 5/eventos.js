7ca90bb892a8bcc33c39f3a4d2c7990a1e444b8a
jhon Aguiar <jhon.aguiar93@gmail.com>
2022-11-21 19:20:40 -0500

    Ajustes

--- clase 5/eventos.js
+++ clase 5/eventos.js
@@ -11,3 +11,38 @@ function cuadrado(){
     // alert(numero * numero)    
     document.getElementById('numero').value = numero * numero
 }
+
+function blure(){
+    document.getElementById('numero').style.background = 'blue'
+    document.getElementById('numero').style.color = 'white'
+}
+
+function focuse() {
+    document.getElementById('numero').style.background = 'red'
+    document.getElementById('numero').style.color = 'pink'
+}
+
+function cambio(){
+    
+    if(document.getElementById('pais').style.background == 'red'){
+        document.getElementById('pais').style.background = 'yellow'
+    }else if(document.getElementById('pais').style.background == 'yellow'){
+        document.getElementById('pais').style.background = 'green'
+    }else{
+        document.getElementById('pais').style.background = 'red'
+    }
+     
+}
+
+function triangulo(){
+    document.getElementById('numero').style.background = 'violet'
+}
+
+function calculameesta(){
+    document.getElementById('numero').style.background = 'orange'
+}
+
+function calculameestas2(){
+    document.getElementById('numero').style.background = 'white'
+}
+
