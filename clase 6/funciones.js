b7adb4eb190af1399d0b067ab035e1b4fd57806e
dmruano <dmruano@bancodeoccidente.com.co>
2022-11-21 20:40:49 -0500

    eventos js

---
+++ clase 6/funciones.js
@@ -0,0 +1,32 @@
+function nombre(e,i,f){
+    return e*i+f;
+}
+
+/*
+crear un boton q calcule si es par o impar!
+*/
+
+function numero(e){
+    
+    if(e % 2 == 0){
+        return  String ="par"
+    }else{
+        return String = "impar"
+    }
+}
+
+
+/*
+    definir una funcion que calcule el numero de caracteres ingresados!
+
+*/
+
+function caracter(){
+
+    let palabra = document.getElementById('caracteres').value
+    
+    return palabra.length;
+
+
+
+}
