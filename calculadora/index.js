5cc32812aeedeec81bffb4b393a0e0b4f9946da1
dmruano <dmruano@bancodeoccidente.com.co>
2022-11-23 21:25:02 -0500

    calculadora

---
+++ calculadora/index.js
@@ -0,0 +1,77 @@
+function capturar1(){
+    let a = document.getElementById('pantalla').value
+    if(a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 1
+}
+
+function capturar2(){
+    let a = document.getElementById('pantalla').value
+    if (a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 2 
+}
+
+
+
+
+
+
+function capturar4(){
+    let a = document.getElementById('pantalla').value
+    if(a == 0){
+        document.getElementById('pantalla').value = '';
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 4
+}
+
+function capturar5(){
+    let a  = document.getElementById('pantalla').value
+    if (a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 5
+}
+
+function capturar6(){
+    let a = document.getElementById('pantalla').value
+    if (a == 0){
+        document.getElementById('pantalla').value = '';
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value  + 6
+}
+
+function capturar7(){
+    let a = document.getElementById('pantalla').value
+    if ( a == 0 ) {
+        document.getElementById('pantalla').value = '';
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 7; 
+}
+
+function capturar8() {
+    let a = document.getElementById('pantalla').value
+    if(a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value =  document.getElementById('pantalla').value + 8
+}
+
+function capturar9(){
+    let a = document.getElementById('pantalla').value
+    if(a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 9 
+}
+
+
+function capturar3(){
+    let a = document.getElementById('pantalla').value = ''
+    if(a == 0){
+        document.getElementById('pantalla').value = ''
+    }
+    document.getElementById('pantalla').value = document.getElementById('pantalla').value + 3
+}
